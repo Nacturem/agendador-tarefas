e7b004c3ffa8e04e79a27f3b2cdf180007bfcb56
Nacturem <gabriel.grohl2006@gmail.com>
2026-02-14 19:08:59 -0300

    update security

--- src/main/java/infrastructure/security/UserDetailsServiceImpl.java
+++ src/main/java/infrastructure/security/UserDetailsServiceImpl.java
@@ -6,8 +6,6 @@ import infrastructure.client.UsuarioClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
